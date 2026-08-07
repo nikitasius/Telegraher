e89f79a16aa16851a9a4fbe99c011e6afa976881
Nikita S. <nikita@saraeff.net>
2026-08-07 11:59:00 +0200

    no subs * will wipe it all later

--- TMessagesProj/src/main/java/org/telegram/messenger/BillingController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BillingController.java
@@ -190,6 +190,7 @@ public class BillingController implements PurchasesUpdatedListener, BillingClien
     }
 
     public boolean startManageSubscription(Context ctx, String productId) {
+        if (true) return false;
         try {
             ctx.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https://play.google.com/store/account/subscriptions?sku=%s&package=%s", productId, ctx.getPackageName()))));
             return true;
