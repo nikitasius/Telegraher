1db88b6a58c0d8c04d63319e915bf4900debb92c
Nikita S. <nikita@saraeff.net>
2026-08-12 15:17:15 +0200

    no ads

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -24821,7 +24821,8 @@ public class ChatActivity extends BaseFragment implements
                             continue;
                         }
                         if (postId < 0) continue;
-                        getMessagesController().ensureMessagesLoaded(did, postId, null);
+//                        getMessagesController().ensureMessagesLoaded(did, postId, null);
+                        markSponsoredAsRead(messageObject);
                     }
                 } catch (Exception e) {
                     FileLog.e(e, false);
@@ -24837,7 +24838,7 @@ public class ChatActivity extends BaseFragment implements
             if (notPushedSponsoredMessages != null) {
                 notPushedSponsoredMessages.clear();
             }
-            processNewMessages(res.messages, false);
+//            processNewMessages(res.messages, false);
         }
     }
 
