afd994b1a7067493cf14ee964607fb7f4556f9ae
Nikita S. <nikita@saraeff.net>
2026-08-12 12:33:13 +0200

    from old graher and green chats * guess to save & ping that secret data has been read. Afaik it was an issue when folks sent secret stuff, graher saved but folks never had reading confirmation

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -18850,6 +18850,7 @@ public class ChatActivity extends BaseFragment implements
                             }
                         }
                         if (canSave) {
+                            sendSecretMessageRead(messageObject, true);
                             if (messageObject.getDocument() != null && !messageObject.isMusic()) {
                                 String mime = messageObject.getDocument().mime_type;
                                 if (mime != null) {
