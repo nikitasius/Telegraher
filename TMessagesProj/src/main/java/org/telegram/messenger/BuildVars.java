1b0453b5dfd6d23e77370c4598b83368e356a8f0
Nikita S. <nikita@saraeff.net>
2026-08-20 21:22:09 +0200

    rollback the fcm token

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -45,9 +45,7 @@ public class BuildVars {
     public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
 
     public static String PLAYSTORE_APP_URL = "https://github.com/nikitasius/Telegraher/releases/latest";
-    public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-2hh8ebmuflsnjoc0kldkfells9rhtfni.apps.googleusercontent.com";
-
-    public static String HUAWEI_APP_ID = "101184875";
+    public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-81kmi3pi84p11ub8hp9a1funsv0rn2p9.apps.googleusercontent.com";
 
     // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
     public static boolean IS_BILLING_UNAVAILABLE = false;
